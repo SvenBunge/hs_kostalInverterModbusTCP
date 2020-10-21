b3b96807f41d868993b2f5484ef35ad92003a56a
Sven Bunge <SvenBunge@users.noreply.github.com>
2020-10-21 07:59:47 +0200

    used simple mapping instead of iterativ programming approach

--- src/10002_kostalmodbus.py
+++ src/10002_kostalmodbus.py
@@ -1,9 +1,9 @@
 # coding: UTF-8
 import logging
-import pymodbus
-from pymodbus.constants import Endian, Defaults
+import pymodbus  # To not delete this module reference!!
+from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadDecoder
-from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+from pymodbus.client.sync import ModbusTcpClient
 
 ##!!!!##################################################################################################
 #### Own written code can be placed above this commentblock . Do not change or delete commentblock! ####
@@ -36,70 +36,75 @@ class Kostalmodbus10002(hsl20_3.BaseModule):
 #### Own written code can be placed after this commentblock . Do not change or delete commentblock! ####
 ###################################################################################################!!!##
 
+        self.DEBUG = self.FRAMEWORK.create_debug_section()
+
+        self.holdingRegister = {
+            "batterySOC": ['u16', 514, self.PIN_O_BATTERY_SOC, 0],
+            "batteryCONSUMPTION": ['f32', 106, self.PIN_O_HOME_CONSUMPTION_BATTERY, 0.0],
+            "gridCONSUMPTION": ['f32', 108, self.PIN_O_HOME_CONSUMPTION_GRID, 0.0],
+            "pvCONSUMPTION": ['f32', 116, self.PIN_O_HOME_CONSUMPTION_PV, 0.0],
+            "gridPOWER": ['f32', 252, self.PIN_O_TOTAL_POWER_FROM_GRID, 0.0],
+            "inverterPOWER": ['f32', 575, self.PIN_O_INVERTER_POWER, 0.0],
+            "batteryPOWER": ['s16', 582, self.PIN_O_INVERTER_POWER, 0]
+        }
+
+#############
 
     def read_current_power_values(self, ip_address, port_number, unit_id):
- #       client = None
- #       try:
-            self.DEBUG.set_value("creating client with Defaults:", Defaults.Timeout)
+        client = None
+        try:
             self.DEBUG.set_value("creating client with IP:", ip_address)
             self.DEBUG.set_value("creating client with Port:", port_number)
             self.DEBUG.set_value("creating client with UnitID:", unit_id)
-            client = ModbusClient('192.168.80.20', port=1502)
+            client = ModbusTcpClient(ip_address, port=port_number)
             client.connect()
-            self.DEBUG.set_value("client connection established", "yes")
-            ###
-            ## read power values
-            ###
+
             self.read_power_values(client, unit_id)
-#        except Exception as e:
-#            print(e)
-#        finally:
-#            if client:
-#                client.close()
+        finally:
+            if client:
+                client.close()
 
     def read_power_values(self, client, unit_id):
-        # Battery SOC (% - unsigned 16bit int)
-        home_battery_soc = self.read_u16_1(client, unit_id, 514)
-        self.DEBUG.set_value('batterySOC', home_battery_soc)
-        self._set_output_value(self.PIN_O_BATTERY_SOC, home_battery_soc)
 
-        # # Home own consumption from battery (W - 32bit float)
-        home_consumption_battery = self.read_32float_2(client, unit_id, 106)
-        self.DEBUG.set_value('batteryCONSUMPTION', home_consumption_battery)
-        self._set_output_value(self.PIN_O_HOME_CONSUMPTION_BATTERY, home_consumption_battery)
-        #
-        # # Home own consumption from grid (W - 32bit float)
-        home_consumption_grid = self.read_32float_2(client, unit_id, 108)
-        self.DEBUG.set_value('gridCONSUMPTION', home_consumption_grid)
-        self._set_output_value(self.PIN_O_HOME_CONSUMPTION_GRID, home_consumption_grid)
-        #
-        # # Home own consumption from PV (W - 32bit float)
-        home_consumption_pv = self.read_32float_2(client, unit_id, 116)
-        self.DEBUG.set_value('pvCONSUMPTION', home_consumption_pv)
-        self._set_output_value(self.PIN_O_HOME_CONSUMPTION_PV, home_consumption_pv)
-        #
-        # # Calculate current total home consumption
-        home_consumption = home_consumption_battery + home_consumption_grid + home_consumption_pv
-        self.DEBUG.set_value('totalCONSUMPTION', home_consumption)
-        self._set_output_value(self.PIN_O_HOME_CONSUMPTION_TOTAL, home_consumption)
-        #
-        total_power = self.read_32float_2(client, unit_id, 252)
-        self.DEBUG.set_value('totalPOWER', total_power)
-        self._set_output_value(self.PIN_O_TOTAL_POWER_FROM_GRID, total_power)
-        #
-        inverter_power = self.read_32float_2(client, unit_id, 575)
-        self.DEBUG.set_value('inverterPOWER', inverter_power)
-        self._set_output_value(self.PIN_O_INVERTER_POWER, inverter_power)
-        #
-        battery_power = self.read_s16_1(client, unit_id, 582)
-        self.DEBUG.set_value('batteryPOWER', battery_power)
-        self._set_output_value(self.PIN_O_POWER_FROM_BATTERY, battery_power)
-        #
-        total_power_from_pv = battery_power + total_power + home_consumption_pv
+        # fetch all registers
+        for register in self.holdingRegister:
+            value = 0
+            if self.holdingRegister[register][0] == 'u16':
+                value = Kostalmodbus10002.read_u16_1(client, unit_id, self.holdingRegister[register][1])
+            elif self.holdingRegister[register][0] == 's16':
+                value = Kostalmodbus10002.read_s16_1(client, unit_id, self.holdingRegister[register][1])
+            elif self.holdingRegister[register][0] == 'f32':
+                value = Kostalmodbus10002.read_32float_2(client, unit_id, self.holdingRegister[register][1])
+
+            self.DEBUG.set_value(register, value)  # set Debug value
+            self._set_output_value(self.holdingRegister[register][2], value)  # set value to output PIN
+            self.holdingRegister[register][3] = value  # assign value to variable
+
+        # Calculate current total home consumption
+        total_consumption = self.holdingRegister['batteryCONSUMPTION'][3] + self.holdingRegister['gridCONSUMPTION'][3] + self.holdingRegister['pvCONSUMPTION'][3]
+        self.DEBUG.set_value('totalCONSUMPTION', total_consumption)
+        self._set_output_value(self.PIN_O_HOME_CONSUMPTION_TOTAL, total_consumption)
+
+        # Calculate total power from PV
+        total_power_from_pv = min(0, self.holdingRegister['batteryPOWER'][3]) + min(0, self.holdingRegister['gridPOWER'][3]) + self.holdingRegister['pvCONSUMPTION'][3]
         self.DEBUG.set_value('pvPOWER', total_power_from_pv)
         self._set_output_value(self.PIN_O_TOTAL_POWER_FROM_PV, total_power_from_pv)
 
-    def read_u16_1(self, client, unit_id, reg_addr):
+    #############
+
+    def on_init(self):
+        pass
+
+    def on_input_value(self, index, value):
+        if index == self.PIN_I_TRIGGER:
+            ip_address = str(self._get_input_value(self.PIN_I_INVERTER_IP))
+            port = int(self._get_input_value(self.PIN_I_PORT))
+            unit_id = int(self._get_input_value(self.PIN_I_UNIT_ID))
+            self.read_current_power_values(ip_address, port, unit_id)
+
+
+    @staticmethod
+    def read_u16_1(client, unit_id, reg_addr):
         result = client.read_holding_registers(reg_addr, 1, unit=unit_id)
         if not result.isError():
             return BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Big,
@@ -107,7 +112,8 @@ class Kostalmodbus10002(hsl20_3.BaseModule):
         else:
             return -1
 
-    def read_s16_1(self, client, unit_id, reg_addr):
+    @staticmethod
+    def read_s16_1(client, unit_id, reg_addr):
         result = client.read_holding_registers(reg_addr, 1, unit=unit_id)
         if not result.isError():
             return BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Big,
@@ -115,21 +121,11 @@ class Kostalmodbus10002(hsl20_3.BaseModule):
         else:
             return -1
 
-    def read_32float_2(self, client, unit_id, reg_addr):
+    @staticmethod
+    def read_32float_2(client, unit_id, reg_addr):
         result = client.read_holding_registers(reg_addr, 2, unit=unit_id)
         if not result.isError():
             return BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Big,
                                                   wordorder=Endian.Little).decode_32bit_float()
         else:
             return -1
-
-    def on_init(self):
-        self.DEBUG = self.FRAMEWORK.create_debug_section()
-
-    def on_input_value(self, index, value):
-        if index == self.PIN_I_TRIGGER:
-            ip_address = str(self._get_input_value(self.PIN_I_INVERTER_IP))
-            port = int(self._get_input_value(self.PIN_I_PORT))
-            unit_id = int(self._get_input_value(self.PIN_I_UNIT_ID))
-            self.DEBUG.set_value("IP:", ip_address)
-            self.read_current_power_values(ip_address, port, unit_id)
