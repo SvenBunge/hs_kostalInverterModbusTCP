2e6e4033f2536fb7db7ccb5e8d8cabb6ff642142
Sven Bunge <SvenBunge@users.noreply.github.com>
2020-11-23 00:22:44 +0100

    last changes before we release it to the wild

---
+++ buildRelease.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+(cd ../../; python2 ./generator.pyc hs_kostal_modbusTCP utf-8)
+markdown2 --extras tables,fenced-code-blocks,strike,target-blank-links doc/log14180.md > release/log14180.html
+(cd release; zip -r 14180_kostalInverterModbusTCP.hslz *)
